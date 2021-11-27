f0c14e902fc41403d051f4d0aa828c1f36b03ae1
Abhisheek Hayagreev <Abhisheek.Hayagreev@genzeon.com>
2021-11-28 02:00:51 +0530

    Added Comment

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -8,6 +8,6 @@ namespace API.DTOs
     public class UserDto
     {
         public string Username { get; set; }
-        public string Token { get; set; }
+        public string Token { get; set; }//Token
     }
 }
